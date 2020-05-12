ac673c80e448084b4ba9ce802ca5db0efae14b7f
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-12 21:57:45 +0000

    fixed gemspec and added scraper links

---
+++ lib/storm_data/scraper.rb
@@ -0,0 +1,12 @@
+class Scraper 
+  
+  def self.scrape_page 
+    atlc = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"))
+    epac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=5"))
+    cpac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=5"))
+    binding.pry
+  end 
+  
+  
+  
+end 
