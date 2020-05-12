3f01f8c45461d7380aaaf124d691ffbf69c62468
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-12 20:46:40 +0000

    more requirements

--- lib/storm_data.rb
+++ lib/storm_data.rb
@@ -1,5 +1,9 @@
 require_relative "./storm_data/version"
 require_relative "./storm_data/cli"
+require_relative "./storm_data/scraper"
+require "nokogiri"
+require "pry"
+require "open-uri"
 
 module StormData
   class Error < StandardError; end
