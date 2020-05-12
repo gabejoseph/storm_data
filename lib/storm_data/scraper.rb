a62c0a68f25fc1015783dac0688752e17bfe8973
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-12 22:17:33 +0000

    expanding scraper, parsing sites

--- lib/storm_data/scraper.rb
+++ lib/storm_data/scraper.rb
@@ -1,12 +1,27 @@
 class StormData::Scraper 
   
-  def self.scrape_page 
+  @@all = []
+  
+  def self.scrape_atlc
     atlc = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"))
+    atlc.css("button").each do |x|
+      x
+      binding.pry
+    end
+  end 
+  
+  def self.scrape_epac
     epac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=5"))
+  end 
+  
+  def self.scrape_cpac 
     cpac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=5"))
-    binding.pry
   end 
   
   
+# /html/body/div[5]/div/table[3]/tbody/tr/td/table[1]/tbody/tr/td/button[1] - Disturbances:
+# /html/body/div[5]/div/table[3]/tbody/tr/td/table[1]/tbody/tr/td/button[2] - None or all[1]  
+# /html/body/div[5]/div/table[5]/tbody/tr/td[2]/button - Tropical Weather Discussion
+
   
 end 
