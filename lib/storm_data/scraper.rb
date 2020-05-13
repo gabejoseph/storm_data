35b32abef3899044e804eb6616ff8ad10cedb51b
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-13 15:18:16 +0000

    adding Scraper/CLI collaboration

--- lib/storm_data/scraper.rb
+++ lib/storm_data/scraper.rb
@@ -6,6 +6,9 @@ class StormData::Scraper
     atlc = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"))
     epac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=5"))
     cpac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=5"))
+    
+    placeholder = StormData::CLI.all 
+
     atlc.css("button").each do |x|
       x
       binding.pry
