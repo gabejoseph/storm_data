e11002a8d143f896ceae8f4062f2ad2f866afccc
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-13 14:17:22 +0000

    basic functionality to CLI class

--- lib/storm_data/cli.rb
+++ lib/storm_data/cli.rb
@@ -1,11 +1,18 @@
 class StormData::CLI 
   
   def call 
-    puts "Welcome to storm chasers"
+    puts "Welcome to Storm Chasers!"
+    puts "Which region would you like to check for activity?"
+    puts "Atlantic, Eastern Pacific or Central Pacific?"
+    region = gets.strip
+    #Method1(region)
   end 
   
-  
-  
-  
-  
+  def method1(region)
+    #approrpiate data from scraper
+    #returns disturbance and info on disturbance in interpolated string
+    #"Would you like to check a different area?"
+    #if yes call again, if no "Thanks for using storm chasers!""
+  end 
+    
 end
