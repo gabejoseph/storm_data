a380d874ad4765d929e2841d40dd53e55edc9b6c
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-13 15:07:16 +0000

    adding in class collaboration

--- lib/storm_data/cli.rb
+++ lib/storm_data/cli.rb
@@ -1,33 +1,50 @@
 class StormData::CLI 
   
+  attr_accessor :region
+  
+  @@all = []
+  
   def call 
+    @region = region
     puts "Welcome to Storm Chasers!"
     puts "Which region would you like to check for activity?"
     puts "Atlantic, Eastern Pacific or Central Pacific?"
-    region = gets.strip
-    if "Atlantic" || "Eastern Pacific" || "Central Pacific"
+    @region = gets.strip
+    if region == "Atlantic" || region == "Eastern Pacific" || region == "Central Pacific"
       #Method1(region)
+      @@all << self 
     else 
       puts "Invalid response, please enter 'Atlantic, Eastern Pacific or Central Pacific."
       call 
     end 
   end 
   
-  def method1(region)
-    #approrpiate data from scraper
-    #returns disturbance and info on disturbance in interpolated string
-    puts "Would you like to check a different area?"
-    puts "If so, please type 'yes' to continue, or 'exit' to end program"
-    finalresponse = gets.strip
-    if finalresponse == "yes" 
-      call
-    elsif finalresponse == "exit"
-      if no "Thanks for using storm chasers!"
-    else 
-      puts "invalid response"
-      call 
-    end 
+  def self.all 
+    @@all
   end 
+  
+  def self.clear 
+    @all.clear 
+  end 
+  
+  
+  
+#   def method1(region)
+#     #approrpiate data from scraper
+#     #returns disturbance and info on disturbance in interpolated string
+#     puts "Would you like to check a different area?"
+#     puts "If so, please type 'yes' to continue, or 'exit' to end program"
+#     finalresponse = gets.strip
+#     if finalresponse == "yes" 
+#       call
+#     elsif finalresponse == "exit"
+#       if no "Thanks for using storm chasers!"
+#        @@all.clear
+#     else 
+#       puts "invalid response"
+#       call 
+#     end 
+#   end 
   
   
     
--- lib/storm_data/scraper.rb
+++ lib/storm_data/scraper.rb
@@ -2,22 +2,17 @@ class StormData::Scraper
   
   @@all = []
   
-  def self.scrape_atlc
+  def self.scrape
     atlc = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=atlc&fdays=5"))
+    epac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=5"))
+    cpac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=5"))
     atlc.css("button").each do |x|
       x
       binding.pry
     end
   end 
-  
-  def self.scrape_epac
-    epac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=epac&fdays=5"))
-  end 
-  
-  def self.scrape_cpac 
-    cpac = Nokogiri::HTML(open("https://www.nhc.noaa.gov/gtwo.php?basin=cpac&fdays=5"))
-  end 
-  
+
+  # @@all.clear after scrape
   
 # /html/body/div[5]/div/table[3]/tbody/tr/td/table[1]/tbody/tr/td/button[1] - Disturbances:
 # /html/body/div[5]/div/table[3]/tbody/tr/td/table[1]/tbody/tr/td/button[2] - None or all[1]  
