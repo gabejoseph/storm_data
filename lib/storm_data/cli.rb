089c6acc42ef54143d79530e5322c08f2728faed
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-05-14 21:57:22 +0000

    working on call method

--- lib/storm_data/cli.rb
+++ lib/storm_data/cli.rb
@@ -1,13 +1,15 @@
 class StormData::CLI 
   
-  attr_accessor :region
+  attr_accessor :region, :prelim
   
   @@all = []
   
   def call 
     @region = gets.strip
     if @region == "Atlantic" || @region == "Eastern Pacific" || @region == "Central Pacific"
-      StormData::Scraper.new.scrape_prelim(@region)
+      variable = StormData::Scraper.new.scrape_prelim(@region)[0][1]
+      "None" == StormData::Scraper.new.scrape_prelim(@region)[0][1]
+      puts "#{variable[0]} #{variable[1]}"
       StormData::Scraper.add_extra(@region)
       @@all << self 
     else 
